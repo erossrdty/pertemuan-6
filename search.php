1354b5a277344c0777eeebb02e7c6c19e61a916c
erossrdty <89617257+erossrdty@users.noreply.github.com>
2022-04-15 22:39:07 +0700

    Initial commit

---
+++ search.php
@@ -0,0 +1,27 @@
+<?php
+    if (isset($_POST['search'])):
+        require_once 'db.php';
+        $search = $_POST['search'];
+
+        $query = mysqli_query($db, "SELECT * FROM film WHERE judul LIKE '%" . $search . "%'");
+        while ($row = mysqli_fetch_object($query)):
+?>
+      <div class="col-sm-auto mt-3">
+          <div class="card" style="width: 15rem"> 
+            <div class="card">
+              <img src="<?= $row->gambar; ?>" class="card-img-top" alt="" style="width: 100%; height: 300pt;" class="hover-shadow"/>
+              <span class="position-absolute top 0 badge" style="background-color: cadetblue"><?= $row->rating; ?></span>
+              <div class="container py-3">
+                <h5 class="card-title"><?= $row->judul; ?> </h5>
+                <p class="card-text">
+                  <button class="btn" style="float: right; font-size: small"><i class="bi bi-pencil-square" style="color:white;"></i>
+                  <i class="bi bi-trash" style="color:white;"></i></button><?= $row->tahun; ?> </p>
+                  <p><button class="button">Watch Now</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+<?php
+        endwhile;
+    endif;
+?>
